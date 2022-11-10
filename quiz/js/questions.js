bf80c06592e0d90443d865360688b0eb603c8c2f
Bouchiba Soulayma <84313061+soulayma-bouchiba@users.noreply.github.com>
2022-11-10 13:18:53 +0000

    Add files via upload

---
+++ quiz/js/questions.js
@@ -0,0 +1,117 @@
+// creating an array and passing the number, questions, options, and answers
+let questions = [
+    {
+    numb: 1,
+    question: "What does HTML stand for?",
+    answer: "Hyper Text Markup Language",
+    options: [
+      "Hyper Text Preprocessor",
+      "Hyper Text Markup Language",
+      "Hyper Text Multiple Language",
+      "Hyper Tool Multi Language"
+    ]
+  },
+    {
+    numb: 2,
+    question: "What does CSS stand for?",
+    answer: "Cascading Style Sheet",
+    options: [
+      "Common Style Sheet",
+      "Colorful Style Sheet",
+      "Computer Style Sheet",
+      "Cascading Style Sheet"
+    ]
+  },
+    {
+    numb: 3,
+    question: "What does PHP stand for?",
+    answer: "Hypertext Preprocessor",
+    options: [
+      "Hypertext Preprocessor",
+      "Hypertext Programming",
+      "Hypertext Preprogramming",
+      "Hometext Preprocessor"
+    ]
+  },
+    {
+    numb: 4,
+    question: "What does SQL stand for?",
+    answer: "Structured Query Language",
+    options: [
+      "Stylish Question Language",
+      "Stylesheet Query Language",
+      "Statement Question Language",
+      "Structured Query Language"
+    ]
+  },
+    {
+    numb: 5,
+    question: "What does XML stand for?",
+    answer: "eXtensible Markup Language",
+    options: [
+      "eXtensible Markup Language",
+      "eXecutable Multiple Language",
+      "eXTra Multi-Program Language",
+      "eXamine Multiple Language"
+    ]
+  },
+   {
+    numb: 6,
+    question: "What is the correct command to create a new React project?",
+     answer: "npx create-react-app myReactApp",
+    options: [
+     "npm create- react-app myReactApp",
+     "npm create-react-app",
+     "npx create-react-app",
+     "npx create-react-app myReactApp"
+    ]
+   },
+
+   {
+    numb: 7,
+    question: "What is the correct file extension for Python files?",
+     answer: ".py",
+    options: [
+     ".pyt" ,
+     ".pyth",
+     ".py",
+     ".pt"
+    ]
+   },
+
+   {
+    numb: 8,
+    question: "What is Git?",
+     answer: "A version control system.",
+    options: [
+     "A version control system.",
+     "A nickname for GitHub.",
+     " A remote repository platform.",
+     "A programming language."
+    ]
+   },
+
+   {
+    numb: 9,
+    question: "The Bootstrap grid system is based on how many columns?",
+     answer: "12",
+    options: [
+     "6",
+     "3",
+     "12",
+     "9"
+    ]
+   },
+
+   {
+    numb: 10,
+    question: "Which jQuery method is used to hide selected elements?",
+     answer: "display(none)",
+    options: [
+     "hidden()",
+     "hide()",
+     "display(none)",
+      "visible(false)"
+    ]
+   },
+];
